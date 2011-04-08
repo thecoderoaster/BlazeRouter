5d26bb3b6277dc0830219e0eac99697c8d95c7a2
unknown <kor@.(none)>
2011-04-07 20:23:31 -0700

    BlazeRouter has been upgraded to work only on Xilinx ISE 12.4. Please take note of this change if you're using an older version, as the project will not open. Added new definitions for the Look up table.

--- BlazePackage.vhd
+++ BlazePackage.vhd
@@ -13,6 +13,7 @@ package router_library is
 	--Universal constants Go Here (these are things that determine dimensions and can be changed on the fly)
 	constant WIDTH		: integer := 7;
 	constant SIZE		: integer := 63;
+	constant LUT_SIZE : integer := 7;			--Start with 8 bits (move up eventually) (KVH)
 	
 	--Buffer Status Codes Go Here
 	constant FULL_FIFO				: std_logic_vector (1 downto 0) := "10";		-- CODE: 0x02 = FIFO full
@@ -23,12 +24,18 @@ package router_library is
 	--Definition of a flit
 	subtype flit 		is std_logic_vector(WIDTH downto 0);	
 	type	fifoBuf		is array (0 to SIZE) of flit;
+	type  t_ram 		is array (0 to 7) of flit;
 	
 	--Subtypes
-	subtype bit8 is std_logic_vector(7 downto 0);
-	subtype bit16 is std_logic_vector(15 downto 0);
-	subtype bit32 is std_logic_vector(31 downto 0);
-	subtype bit64 is std_logic_vector(63 downto 0);
-	subtype bit80 is std_logic_vector(79 downto 0);
+	subtype bit8 		is std_logic_vector(7 downto 0);
+	subtype bit16 		is std_logic_vector(15 downto 0);
+	subtype bit32 		is std_logic_vector(31 downto 0);
+	subtype bit64 		is std_logic_vector(63 downto 0);
+	subtype bit80 		is std_logic_vector(79 downto 0);
+	
+	--Definition of LUTs
+	type	t_ram8		is array (0 to LUT_SIZE) of std_logic_vector(7 downto 0);
+	type	t_ram16		is array (0 to LUT_SIZE) of std_logic_vector(15 downto 0);
+	type	t_ram32		is array (0 to LUT_SIZE) of std_logic_vector(31 downto 0);
 	
 end router_library;
